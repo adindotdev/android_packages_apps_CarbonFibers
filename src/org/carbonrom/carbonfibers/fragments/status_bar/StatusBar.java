0fe304cd7e1fc51995f81a723618c5c3e71e3876
Lars Greiss <kufikugel@googlemail.com>
2018-08-18 00:32:52 -0700

    CF: status bar clock date customization (2/2)
    Change-Id: I461125b7e2e05b2d1e274745ea8fbca4d366182d
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- src/org/carbonrom/carbonfibers/fragments/status_bar/StatusBar.java
+++ src/org/carbonrom/carbonfibers/fragments/status_bar/StatusBar.java
@@ -17,17 +17,141 @@
 package org.carbonrom.carbonfibers.fragments.status_bar;
 
 import android.os.Bundle;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.text.format.DateFormat;
+import android.widget.EditText;
 
 import com.android.settings.R;
+import com.android.settings.Utils;
 import com.android.settings.carbon.CustomSettingsPreferenceFragment;
 
-public class StatusBar extends CustomSettingsPreferenceFragment {
+import java.util.Date;
+
+public class StatusBar extends CustomSettingsPreferenceFragment implements
+	    OnPreferenceChangeListener {
     private static final String TAG = "StatusBar";
+    private static final String STATUS_BAR_CLOCK = "status_bar_clock";
+    private static final String STATUS_BAR_CLOCK_SECONDS = "status_bar_clock_seconds";
+    private static final String STATUS_BAR_CLOCK_STYLE = "statusbar_clock_style";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+    private static final String STATUS_BAR_CLOCK_DATE_DISPLAY = "clock_date_display";
+    private static final String STATUS_BAR_CLOCK_DATE_STYLE = "clock_date_style";
+    private static final String STATUS_BAR_CLOCK_DATE_FORMAT = "clock_date_format";
+    private static final int CLOCK_DATE_STYLE_LOWERCASE = 1;
+    private static final int CLOCK_DATE_STYLE_UPPERCASE = 2;
+    private static final int CUSTOM_CLOCK_DATE_FORMAT_INDEX = 18;
+
+    private ListPreference mClockDateFormat;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.status_bar);
+        addCustomPreference(findPreference(STATUS_BAR_CLOCK), SYSTEM_TWO_STATE, STATE_ON);
+        addCustomPreference(findPreference(STATUS_BAR_CLOCK_SECONDS), SYSTEM_TWO_STATE, STATE_OFF);
+
+        updateTwentyFourHourFormat();
+
+        mClockDateFormat = (ListPreference) findPreference(STATUS_BAR_CLOCK_DATE_FORMAT);
+        mClockDateFormat.setOnPreferenceChangeListener(this);
+        String value = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.STATUSBAR_CLOCK_DATE_FORMAT);
+        if (value == null || value.isEmpty()) {
+            value = "EEE";
+        }
+        int index = mClockDateFormat.findIndexOfValue((String) value);
+        if (index == -1) {
+            mClockDateFormat.setValueIndex(CUSTOM_CLOCK_DATE_FORMAT_INDEX);
+        } else {
+            mClockDateFormat.setValue(value);
+        }
+        parseClockDateFormats();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (mClockDateFormat.equals(preference)) {
+            int index = mClockDateFormat.findIndexOfValue((String) newValue);
+            if (index == CUSTOM_CLOCK_DATE_FORMAT_INDEX) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+                alert.setTitle(R.string.clock_date_string_edittext_title);
+                alert.setMessage(R.string.clock_date_string_edittext_summary);
+                final EditText input = new EditText(getActivity());
+                String oldText = Settings.System.getString(
+                    getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_DATE_FORMAT);
+                if (oldText != null) {
+                    input.setText(oldText);
+                }
+                alert.setView(input);
+                alert.setPositiveButton(R.string.menu_save, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int whichButton) {
+                        String value = input.getText().toString();
+                        if (value.equals("")) {
+                            return;
+                        }
+                        Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.STATUSBAR_CLOCK_DATE_FORMAT, value);
+                        return;
+                    }
+                });
+                alert.setNegativeButton(R.string.menu_cancel,
+                    new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        return;
+                    }
+                });
+                AlertDialog dialog = alert.create();
+                dialog.show();
+            } else {
+                if ((String) newValue != null) {
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.STATUSBAR_CLOCK_DATE_FORMAT, (String) newValue);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void updateTwentyFourHourFormat() {
+        if (DateFormat.is24HourFormat(getActivity())) {
+            findPreference(STATUS_BAR_AM_PM).setEnabled(false);
+            findPreference(STATUS_BAR_AM_PM).setSummary(R.string.status_bar_am_pm_info);
+        }
+    }
+
+    private void parseClockDateFormats(SystemSettingListPreference clockDateFormat) {
+        String[] dateEntries = getResources().getStringArray(R.array.clock_date_format_entries_values);
+        CharSequence parsedDateEntries[];
+        parsedDateEntries = new String[dateEntries.length];
+        Date now = new Date();
+
+        int lastEntry = dateEntries.length - 1;
+        int dateFormat = Settings.System.getInt(getActivity().getContentResolver(),         
+                Settings.System.STATUSBAR_CLOCK_DATE_STYLE, 0);
+        for (int i = 0; i < dateEntries.length; i++) {
+            if (i == lastEntry) {
+                parsedDateEntries[i] = dateEntries[i];
+            } else {
+                String newDate;
+                CharSequence dateString = DateFormat.format(dateEntries[i], now);
+                if (dateFormat == CLOCK_DATE_STYLE_LOWERCASE) {
+                    newDate = dateString.toString().toLowerCase();
+                } else if (dateFormat == CLOCK_DATE_STYLE_UPPERCASE) {
+                    newDate = dateString.toString().toUpperCase();
+                } else {
+                    newDate = dateString.toString();
+                }
+                 parsedDateEntries[i] = newDate;
+            }
+        }
+        clockDateFormat.setEntries(parsedDateEntries);
     }
 }
